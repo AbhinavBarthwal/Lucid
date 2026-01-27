9e1b83f4b344a6885fa8ef7d06fecfb744e34475
AbhinavBarthwal <abhinavbarthwal12345@gmail.com>
2026-01-27 14:14:18 +0530

    other fixes

--- Lucid/EyeExercisesTableViewController.swift
+++ Lucid/EyeExercisesTableViewController.swift
@@ -16,5 +16,24 @@ class ExerciseTableViewController: UITableViewController {
         
         // 4. Ensure transparency of the table and cells
         self.tableView.backgroundColor = .clear
+        
+
+            // This finds every label with tag 10 (Titles) and 20 (Descriptions)
+            view.subviews.forEach { subview in
+                // Fix Titles
+                if let title = subview.viewWithTag(10) as? UILabel {
+                    title.font = UIFont.preferredFont(forTextStyle: .headline)
+                    title.setContentHuggingPriority(.defaultHigh, for: .vertical)
+                }
+                
+                // Fix Descriptions
+                if let desc = subview.viewWithTag(20) as? UILabel {
+                    desc.numberOfLines = 0
+                    desc.setContentHuggingPriority(.defaultLow, for: .vertical)
+                }
+            }
+        
     }
+    // In your ExerciseTableViewController.swift
+  
 }
